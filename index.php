2e2c0c1bbe328410a9112f6b68ae4f4864121377
Allen <1932005951@qq.com>
2019-05-05 02:42:14 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<link href="main.css" rel="stylesheet" type="text/css">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<script>
+	function toggle(){
+		m=document.getElementById('music');
+		if(m.paused){
+			m.play();
+		}else{
+			m.pause();
+		}
+	}
+</script>
+<body>
+<audio id="music"src="euterupe.mp3" autoplay="autoplay" loop="loop" ></audio>
+	<h2 style="text-align:center" onclick="toggle()">エウテルペ</h2>
+
+	<p><ruby>咲<rt>さ</rt></ruby>いた<ruby>野<rt>の</rt></ruby>の<ruby>花<rt>はな</rt></ruby>よ</p>
+	<p>ああ　どうか<ruby>教<rt>おし</rt></ruby>えておくれ</p>
+	<p><ruby>人<rt>ひと</rt></ruby>は<ruby>何故<rt>なぜ</rt></ruby>　<ruby>傷<rt>きず</rt></ruby>つけあって</p>
+	<p><ruby>争<rt>あらそ</rt></ruby>うのでしょう</p>
+
+	<p><ruby>凛<rt>りん</rt></ruby>と<ruby>咲<rt>さ</rt></ruby>く<ruby>花<rt>はな</rt></ruby>よ</p>
+	<p>そこから何が見える</p>
+	<p>人は<ruby>何故<rt>なぜ</rt></ruby>　<ruby>許<rt>ゆる</rt></ruby>しあうこと</p>
+	<p>できないのでしょう</p>
+
+	<p><ruby>雨<rt>あめ</rt></ruby>が<ruby>過<rt>す</rt>ぎ</ruby>て<ruby>夏<rt>なつ</rt></ruby>は</p>
+	<p><ruby>青<rt>あお</rt></ruby>を<ruby>移<rt>うつ</rt></ruby>した</p>
+	<p><ruby>一<rt>ひと</rt></ruby>つになって</p>
+	<p>小さく<ruby>揺<rt>ゆ</rt></ruby>れた</p>
+	<p>私の<ruby>前<rt>まえ</rt></ruby>で</p>
+	<p>何も言わずに</p>
+
+	<p><ruby>枯<rt>か</rt></ruby>れていく<ruby>友<rt>とも</rt></ruby>に</p>
+	<p>お前は何を<ruby>思<rt>おも</rt></ruby>う</p>
+	<p><ruby>言葉<rt>ことば</rt></ruby>を<ruby>持<rt>も</rt></ruby>たぬその<ruby>葉<rt>は</rt></ruby>で</p>
+	<p>なんと<ruby>愛<rt>あい</rt></ruby>を<ruby>伝<rt>つた</rt></ruby>える</p>
+
+	<p><ruby>夏<rt>なつ</rt></ruby>の<ruby>陽<rt>ひ</rt></ruby>は<ruby>陰<rt>かげ</rt></ruby>って</p>
+	<p><ruby>風<rt>かぜ</rt></ruby>が<ruby>靡<rt>なび</rt></ruby>いた</p>
+	<p><ruby>二<rt>ふた</rt></ruby>つ<ruby>重<rt>かさ</rt></ruby>なって</p>
+	<p><ruby>生<rt>い</rt></ruby>きた<ruby>証<rt>あかし</rt></ruby>を</p>
+	<p>私は<ruby>唄<rt>うた</rt></ruby>おう</p>
+	<p><ruby>名<rt>な</rt></ruby>もなき<ruby>者<rt>もの</rt></ruby>のため</p>
+
+</body>
+</html>
