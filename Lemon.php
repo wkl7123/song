bb5080de79c93d6fce80a4939075efc855f465f4
iver <libaolin@bilibili.com>
2019-05-05 03:21:37 +0800

    lemon

---
+++ Lemon.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<link href="main.css" rel="stylesheet" type="text/css">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<script>
+	function toggle(){
+		m=document.getElementById('music');
+		if(m.paused){
+			m.play();
+		}else{
+			m.pause();
+		}
+	}
+</script>
+<body>
+<audio id="music"src="source/lemon.mp3" autoplay="autoplay" loop="loop" ></audio>
+	<h2 style="text-align:center" onclick="toggle()">エウテルペ</h2>
+
+	<p><ruby>梦<rt>ゆめ</rt></ruby>ならばどれほどよかったでしょう</p>
+<p><ruby>未<rt>いま</rt></ruby>だにあなたのことを<ruby>梦<rt>ゆめ</rt></ruby>にみる</p>
+<p><ruby>忘<rt>わす</rt></ruby>れた<ruby>物<rt>もの</rt></ruby>を<ruby>取<rt>と</rt></ruby>りに<ruby>帰<rt>かえ</rt></ruby>るように</p>
+<p><ruby>古<rt>ふる</rt></ruby>びた<ruby>思<rt>おも</rt></ruby>い<ruby>出<rt>で</rt></ruby>の<ruby>埃<rt>ほこり</rt></ruby>を<ruby>払<rt>はら</rt></ruby>う</p>
+<p><ruby>戻<rt>もど</rt></ruby>らない<ruby>幸<rt>しあわ</rt></ruby>せがあることを</p>
+<p><ruby>最後<rt>さいご</rt></ruby>にあなたが<ruby>教<rt>おし</rt></ruby>えてくれた</p>
+<p><ruby>言<rt>い</rt></ruby>えずに<ruby>隠<rt>かく</rt></ruby>してた<ruby>昏<rt>くら</rt></ruby>い<ruby>过去<rt>かこ</rt></ruby>も</p>
+<p>あなたがいなきゃ<ruby>永远<rt>えいえん</rt></ruby>に<ruby>昏<rt>くら</rt></ruby>いまま</p>
+<p>きっともうこれ<ruby>以上<rt>いじょう</rt></ruby> <ruby>伤<rt>きず</rt></ruby>つくことなど</p>
+<p>ありはしないとわかっている</p>
+<p>あの<ruby>日<rt>ひ</rt></ruby>の<ruby>悲<rt>かな</rt></ruby>しみさえ</p>
+<p>あの<ruby>日<rt>ひ</rt></ruby>の<ruby>苦<rt>くる</rt></ruby>しみさえ</p>
+<p>そのすべてを<ruby>爱<rt>あい</rt></ruby>してた あなたとともに</p>
+<p><ruby>胸<rt>むね</rt></ruby>に<ruby>残<rt>のこ</rt></ruby>り<ruby>离<rt>はな</rt></ruby>れない</p>
+<p><ruby>苦<rt>にが</rt></ruby>い<ruby>レモン<rt>れもん</rt></ruby>の<ruby>匂<rt>にお</rt></ruby>い</p>
+<p><ruby>雨<rt>あめ</rt></ruby>が<ruby>降<rt>ふ</rt></ruby>り<ruby>止<rt>や</rt></ruby>むまでは<ruby>帰<rt>かえ</rt></ruby>れない</p>
+<p><ruby>今<rt>いま</rt></ruby>でもあなたはわたしの<ruby>光<rt>ひかり</rt></ruby></p>
+<p><ruby>暗闇<rt>くらやみ</rt></ruby>であなたの<ruby>背<rt>せ</rt></ruby>をなぞった</p>
+<p>その<ruby>轮廓<rt>りんかく</rt></ruby>を<ruby>鲜明<rt>せんめい</rt></ruby>に<ruby>覚<rt>おぼ</rt></ruby>えている</p>
+<p><ruby>受<rt>う</rt></ruby>け<ruby>止<rt>と</rt></ruby>めきれないものと<ruby>出会<rt>であ</rt></ruby>うたび</p>
+<p><ruby>溢<rt>あふ</rt></ruby>れてやまないのは<ruby>涙<rt>なみだ</rt></ruby>だけ</p>
+<p><ruby>何<rt>なに</rt></ruby>をしていたの</p>
+<p><ruby>何<rt>なに</rt></ruby>を<ruby>见<rt>み</rt></ruby>ていたの</p>
+<p>わたしの<ruby>知<rt>し</rt></ruby>らない<ruby>横颜<rt>よこがお</rt></ruby>で</p>
+<p>どこかであなたが<ruby>今<rt>いま</rt></ruby></p>
+<p>わたしと<ruby>同<rt>おな</rt></ruby>じ<ruby>様<rt>よう</rt></ruby>な</p>
+<p><ruby>涙<rt>なみだ</rt></ruby>にくれ <ruby>淋<rt>さび</rt></ruby>しさの<ruby>中<rt>なか</rt></ruby>にいるなら</p>
+<p>わたしのことなどどうか <ruby>忘<rt>わす</rt></ruby>れてください</p>
+<p>そんなことを<ruby>心<rt>こころ</rt></ruby>から<ruby>愿<rt>ねが</rt></ruby>うほどに</p>
+<p><ruby>今<rt>いま</rt></ruby>でもあなたはわたしの<ruby>光<rt>ひかり</rt></ruby></p>
+<p><ruby>自分<rt>じぶん</rt></ruby>が<ruby>思<rt>おも</rt></ruby>うより <ruby>恋<rt>こい</rt></ruby>をしていたあなたに</p>
+<p>あれから<ruby>思<rt>おも</rt></ruby>うように <ruby>息<rt>いき</rt></ruby>ができない</p>
+<p>あんなに<ruby>侧<rt>そば</rt></ruby>にいたのにまるで<ruby>嘘<rt>うそ</rt></ruby>みたい</p>
+<p>とても<ruby>忘<rt>わす</rt></ruby>れられないそれだけが<ruby>确<rt>たし</rt></ruby>か</p>
+<p>あの<ruby>日<rt>ひ</rt></ruby>の<ruby>悲<rt>かな</rt></ruby>しみさえ</p>
+<p>あの<ruby>日<rt>ひ</rt></ruby>の<ruby>苦<rt>くる</rt></ruby>しみさえ</p>
+<p>その<ruby>全<rt>すべ</rt></ruby>てを<ruby>爱<rt>あい</rt></ruby>してたあなたと<ruby>共<rt>とも</rt></ruby>に</p>
+<p><ruby>胸<rt>むね</rt></ruby>に<ruby>残<rt>のこ</rt></ruby>り<ruby>离<rt>はな</rt></ruby>れない</p>
+<p><ruby>苦<rt>にが</rt></ruby>い<ruby>レモン<rt>れもん</rt></ruby>の<ruby>匂<rt>にお</rt></ruby>い</p>
+<p><ruby>雨<rt>あめ</rt></ruby>が<ruby>降<rt>ふ</rt></ruby>り<ruby>止<rt>や</rt></ruby>むまでは<ruby>帰<rt>かえ</rt></ruby>れない</p>
+<p><ruby>切<rt>き</rt></ruby>り<ruby>分<rt>わ</rt></ruby>けた<ruby>果実<rt>かじつ</rt></ruby>の<ruby>片方<rt>かたほう</rt></ruby>の<ruby>様<rt>よう</rt></ruby>に</p>
+<p><ruby>今<rt>いま</rt></ruby>でもあなたはわたしの<ruby>光<rt>ひかり</rt></ruby></p>
+
+</body>
+</html>
