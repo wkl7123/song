7533f063041790934dd5dddd85f5461e4ac27cc4
iver <libaolin@bilibili.com>
2019-05-18 17:22:38 +0800

    secret base

---
+++ secret_base.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<link href="main.css" rel="stylesheet" type="text/css">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<script>
+	function toggle(){
+		m=document.getElementById('music');
+		if(m.paused){
+			m.play();
+		}else{
+			m.pause();
+		}
+	}
+</script>
+<body>
+<audio id="music"src="" autoplay="autoplay" loop="loop" ></audio>
+	<h2 style="text-align:center" onclick="toggle()">Lemon</h2>
+
+<p><ruby>君<rt>きみ</rt></ruby>と<ruby>夏<rt>なつ</rt></ruby>の<ruby>终<rt>お</rt></ruby>わり <ruby>将来<rt>しょうらい</rt></ruby>の</p><ruby>梦<rt>ゆめ</rt></ruby>
+<p><ruby>大<rt>おお</rt></ruby>きな<ruby>希望<rt>きぼう</rt></ruby> <ruby>忘<rt>わす</rt></ruby>れない</p>
+<p>十年<rt>じゅうねん</rt></ruby><ruby>後<rt>ご</rt></ruby>の<ruby>八月<rt>はちがつ</rt></ruby></p>
+<p>また<ruby>出会<rt>であ</rt></ruby>えるのを <ruby>信<rt>しん</rt></ruby>じて</p>
+<p><ruby>最高<rt>さいこう</rt></ruby>の<ruby>思<rt>おも</rt></ruby>い<ruby>出<rt>で</rt></ruby>を…</p>
+<p><ruby>出会<rt>であ</rt></ruby>いは ふっとした <ruby>瞬间<rt>しゅんかん</rt></ruby> <ruby>帰<rt>かえ</rt></ruby>り<ruby>道<rt>みち</</rt></ruby>の<ruby>交差点<rt>こうさてん</rt></ruby>で
+<p><ruby>声<rt>こえ</rt></ruby>をかけてくれたね 「<ruby>一绪<rt>いっしょ</rt></ruby>に<ruby>帰<rt>かえ</rt></ruby>ろう」</p>
+<p><ruby>仆<rt>ぼく</rt></ruby>は <ruby>照<rt>て</rt></ruby>れくさそうに カバンで<ruby>颜<rt>かお</rt></ruby>を<ruby>隠<rt>かく</rt></ruby></ruby>しながら
+<p><ruby>本当<rt>ほんとう</rt></ruby>は とても とても <ruby>嬉<rt>うれ</rt></ruby>しかったよ</p>
+<p>あぁ <ruby>花火<rt>はなび</rt></ruby>が<ruby>夜空<rt>よぞら</rt></ruby> <ruby>绮丽(きれい)に咲<rt>さ</rt></ruby>いて　ちょっとセツナク</p>
+<p>あぁ <ruby>风<rt>かぜ</rt></ruby>が<ruby>时间<rt>じかん</rt></ruby>とともに <ruby>流<rt>なが</rt></ruby>れる</p>
+<p><ruby>嬉<rt>うれ</rt></ruby>しくって <ruby>楽<rt>たの</rt></ruby>しくって <ruby>冒険<rt>ぼうけん</rt></ruby>も　いろいろしたね</p>
+<p><ruby>二人<rt>ふたり</rt></ruby>の <ruby>秘密<rt>ひみつ</rt></ruby>の <ruby>基地<rt>きち</rt></ruby>の<ruby>中<rt>なか</rt></ruby>>
+<p><ruby>君<rt>きみ</rt></ruby>と<ruby>夏<rt>なつ</rt></ruby>の<ruby>终<rt>お</rt></ruby>わり <ruby>将来<rt>しょうらい</rt></</ruby>の<ruby>梦<rt>ゆめ</rt></ruby><ruby>大<rt>おお</rt></ruby>きな<ruby>希望<rt>きぼう</rt></ruby> <ruby>忘<rt>わす</rt></ruby>れない
+<p><ruby>十年<rt>じゅうねん</rt></ruby><ruby>後<rt>ご</rt></ruby>の<ruby>八月<rt>はちがつ</rt></ruby>また<ruby>出会<rt>であ</rt></</ruby>えるのを <ruby>信<rt>しん</rt></ruby>じて
+<p><ruby>君<rt>きみ</rt></ruby>が<ruby>最後<rt>さいご</rt></ruby>まで <ruby>心<rt>こころ</rt></ruby>から 「ありがとう」<ruby>叫<rt>さけ</rt></rt></ruby>んでいたこと <ruby>知<rt>し</rt></ruby>ってたよ
+<p><ruby>涙<rt>なみだ</rt></ruby>をこらえて <ruby>笑颜<rt>えがお</rt></ruby>でさよなら せつないよね</p>
+<p><ruby>最高<rt>さいこう</rt></ruby>の<ruby>思<rt>おも</rt></ruby>い<ruby>出<rt>で</rt></ruby>を…</p>
+<p>あぁ <ruby>夏休<rt>なつやす</rt></ruby>みも あと<ruby>少<rt>すこ</rt></ruby>しで <ruby>终<rt>お</rt></ruby>わっちゃうから</p>
+<p>あぁ <ruby>太阳<rt>たいよう</rt></ruby>と<ruby>月<rt>つき</rt></ruby> <ruby>仲良<rt>なかよ</rt></ruby>くして</p>
+<p><ruby>悲<rt>かな</rt></ruby>しくって<ruby>寂<rt>さび</rt></ruby>しくって<ruby>喧哗<rt>けんか</rt></ruby>も いろいろしたね</p>
+<p><ruby>二人<rt>ふたり</rt></ruby>の <ruby>秘密<rt>ひみつ</rt></ruby>の <ruby>基地<rt>きち</rt></ruby>の<ruby>中<rt>なか</rt></ruby>>
+<p><ruby>君<rt>きみ</rt></ruby>が<ruby>最後<rt>さいご</rt></ruby>まで <ruby>心<rt>こころ</rt></ruby>から 「ありがとう」<ruby>叫<rt>さけ</rt></rt></ruby>んでいたこと <ruby>知<rt>し</rt></ruby>ってたよ
+<p><ruby>涙<rt>なみだ</rt></ruby>をこらえて <ruby>笑颜<rt>えがお</rt></ruby>でさよなら せつないよね</p>
+<p><ruby>最高<rt>さいこう</rt></ruby>の<ruby>思<rt>おも</rt></ruby>い<ruby>出<rt>で</rt></ruby>を…</p>
+<p><ruby>突然<rt>とつぜん</rt></ruby>の <ruby>転校<rt>てんこう</rt></ruby>で どうしようもなく</p>
+<p><ruby>手纸<rt>てがみ</rt></ruby><ruby>书<rt>か</rt></ruby>くよ <ruby>电话<rt>でんわ</rt></ruby>もするよ <ruby>忘<rt>わす</rt></</ruby>れないでね <ruby>仆<rt>ぼく</rt></ruby>のことを
+<p>いつまでも <ruby>二人<rt>ふたり</rt></ruby>の <ruby>基地<rt>きち</rt></ruby>の<ruby>中<rt>なか</rt></ruby></p>
+<p><ruby>君<rt>きみ</rt></ruby>と<ruby>夏<rt>なつ</rt></ruby>の<ruby>终<rt>お</rt></ruby>わり ずっと<ruby>话<rt>はな</rt></</ruby>して
+<p><ruby>夕日<rt>ゆうひ</rt></ruby>を<ruby>见<rt>み</rt></ruby>てから <ruby>星<rt>ほし</rt></ruby>を<ruby>眺<rt>なが</rt></ruby>め</p>
+<p><ruby>君<rt>きみ</rt></ruby>の<ruby>頬<rt>ほほ</rt></ruby>を <ruby>流<rt>なが</rt></ruby>れた<ruby>涙<rt>なみだ</rt></ruby>はず </p>っと<ruby>忘<rt>わす</rt></ruby>れない
+<p><ruby>君<rt>きみ</rt></ruby>が<ruby>最後<rt>さいご</rt></ruby>まで <ruby>大<rt>おお</rt></ruby>きく<ruby>手<rt>て</rt></ruby>を<ruby>振</ruby><rt>ふ</rt></ruby>ってくれたこと
+<p>きっと<ruby>忘<rt>わす</rt></ruby>れない</p>
+<p>だから こうして <ruby>梦<rt>ゆめ</rt></ruby>の<ruby>中<rt>なか</rt></ruby>で ずっと<ruby>永远<rt>えいえん</rt></ruby>に…</p>
+<p><ruby>君<rt>きみ</rt></ruby>と<ruby>夏<rt>なつ</rt></ruby>の<ruby>终<rt>お</rt></ruby>わり <ruby>将来<rt>しょうらい</rt></ruby>の</p><ruby>梦<rt>ゆめ</rt></ruby><ruby>大<rt>おお</rt></ruby>きな<ruby>希望<rt>きぼう</rt></ruby> <ruby>忘<rt>わす</rt></ruby>れない
+<p><ruby>十年<rt>じゅうねん</rt></ruby><ruby>後<rt>ご</rt></ruby>の<ruby>八月<rt>はちがつ</rt></ruby>また<ruby>出会<rt>であ</rt></</ruby>えるのを <ruby>信<rt>しん</rt></ruby>じて
+<p><ruby>君<rt>きみ</rt></ruby>が<ruby>最後<rt>さいご</rt></ruby>まで <ruby>心<rt>こころ</rt></ruby>から 「ありがとう」<ruby>叫<rt>さけ</rt></rt></ruby>んでいたこと <ruby>知<rt>し</rt></ruby>ってたよ
+<p><ruby>涙<rt>なみだ</rt></ruby>をこらえて <ruby>笑颜<rt>えがお</rt></ruby>でさよなら せつないよね</p>
+<p><ruby>最高<rt>さいこう</rt></ruby>の<ruby>思<rt>おも</rt></ruby>い<ruby>出<rt>で</rt></ruby>を…</p>
+<p><ruby>最高<rt>さいこう</rt></ruby>の<ruby>思<rt>おも</rt></ruby>い<ruby>出<rt>で</rt></ruby>を…</p>
+
+</body>
+</html>
